d9a255eee45022fb83695c15dd0d8c5ceed83e0c
chungnv <chung@Mac.fis.vn>
2026-07-20 14:00:32 +0700

    Fix sliced loading bar progress

--- Assets/_Projects/Scripts/Editor/ToolMenuEditor.cs
+++ Assets/_Projects/Scripts/Editor/ToolMenuEditor.cs
@@ -5,14 +5,20 @@ using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[InitializeOnLoad]
 public static class ToolMenuEditor
 {
+    private const string InitScenePath = "Assets/_Projects/Scenes/Init.unity";
+
+    static ToolMenuEditor()
+    {
+        EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(InitScenePath);
+    }
+
     [MenuItem("MyMenu/" + nameof(StartGame))]
     public static void StartGame()
     {
-        EditorSceneManager.OpenScene("Assets/_Projects/Scenes/Init.unity");
+        EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(InitScenePath);
         EditorApplication.isPlaying = true;
-
     }
 }
-
--- Assets/_Projects/Scripts/UI/PopupLoading/PopupLoading.cs
+++ Assets/_Projects/Scripts/UI/PopupLoading/PopupLoading.cs
@@ -1,18 +1,19 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupLoading : DraftUtils.DraftMonoBehaviour
 {
     [SerializeField] private DraftUtils.Popup popup;
     [SerializeField] private DraftUtils.ImageFilledSlider progressSlider;
+    [SerializeField] private Image progressFillImage;
 
     public void SetData(float duration, Action onComplete = null)
     {
         progressSlider.ValueToDisplayTextFunc = progressSlider.ValueToDisplayTextLoading;
         progressSlider.SetMaxValue(1);
-        progressSlider.SetValue(0);
-        progressSlider.Apply();
+        SetValue(0f);
 
         float value = 0;
         DOTween.To(() => value, x => SetValue(x), 1f, duration)
@@ -28,7 +29,15 @@ public class PopupLoading : DraftUtils.DraftMonoBehaviour
     }
     private void SetValue(float progress)
     {
+        progress = Mathf.Clamp01(progress);
         progressSlider.SetValue(progress);
         progressSlider.Apply();
+
+        var fillRect = progressFillImage.rectTransform;
+        fillRect.anchorMin = Vector2.zero;
+        fillRect.anchorMax = new Vector2(progress, 1f);
+        fillRect.offsetMin = Vector2.zero;
+        fillRect.offsetMax = Vector2.zero;
+        progressFillImage.enabled = progress > 0f;
     }
 }
