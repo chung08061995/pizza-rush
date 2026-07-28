dbb48f40c1d512a98cdd29550f6ed18ca290a975
chungnv <chung@MacBook-Air-cua-Chung.local>
2026-07-28 16:05:39 +0700

    fix: show container movement indicators

--- Assets/_Projects/Scripts/Container/ContainerMovementView.cs
+++ Assets/_Projects/Scripts/Container/ContainerMovementView.cs
@@ -5,6 +5,7 @@ public class ContainerMovementView : DraftUtils.DraftMonoBehaviour
 {
     [SerializeField] private DraftUtils.OptionalValue<GameObject> horizontalDirectionObject = new();
     [SerializeField] private DraftUtils.OptionalValue<GameObject> verticalDirectionObject = new();
+    [SerializeField] private float surfaceOffset = 0.03f;
 
     private ContainerData _data;
 
@@ -13,6 +14,7 @@ public class ContainerMovementView : DraftUtils.DraftMonoBehaviour
         _data = data;
         SetHorizontalDirectionObject();
         SetVerticalDirectionObject();
+        AlignCanvasToContainerBounds();
     }
 
     [Button]
@@ -38,5 +40,39 @@ public class ContainerMovementView : DraftUtils.DraftMonoBehaviour
             return;
         }
         verticalDirectionObject.value.SetActive(_data.containerMovementType == ContainerMovementType.Vertical);
+    }
+
+    private void AlignCanvasToContainerBounds()
+    {
+        var container = GetComponentInParent<Container>();
+        var canvas = GetComponentInParent<Canvas>();
+        if (container == null || canvas == null || canvas.transform is not RectTransform canvasRect)
+        {
+            return;
+        }
+
+        var renderers = container.GetComponentsInChildren<Renderer>();
+        if (renderers.Length == 0)
+        {
+            return;
+        }
+
+        var bounds = renderers[0].bounds;
+        for (var i = 1; i < renderers.Length; i++)
+        {
+            bounds.Encapsulate(renderers[i].bounds);
+        }
+
+        canvasRect.position = new Vector3(bounds.center.x, bounds.max.y + surfaceOffset, bounds.center.z);
+        canvasRect.rotation = Quaternion.Euler(90f, 0f, 0f);
+
+        var scaleX = Mathf.Abs(canvasRect.lossyScale.x);
+        var scaleY = Mathf.Abs(canvasRect.lossyScale.y);
+        if (scaleX <= Mathf.Epsilon || scaleY <= Mathf.Epsilon)
+        {
+            return;
+        }
+
+        canvasRect.sizeDelta = new Vector2(bounds.size.x / scaleX, bounds.size.z / scaleY);
     }
 }
